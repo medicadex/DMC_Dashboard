88fe96b6d7ce26598f39f74c2b32db95b9fe44dc
medicadex <165914361+medicadex@users.noreply.github.com>
2026-04-08 08:20:10 +0100

    Updated
    scroll bar in the search bar and address search

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def api_account_dashboard(account_number):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
